ae43b5241e2126af99c232210b4e45f347d54414
Jude D'Souza <dsouza.jude936@gmail.com>
2021-03-30 22:55:43 -0400

    added dark theme to tables and starting collapsable button

--- tables.js
+++ tables.js
@@ -1,22 +1,25 @@
-async function getTable(){
+async function getTable() {
     data = await getData();
     //document.getElementById("demo").innerHTML = data.pcea[0][0];
 
     var table_pcea = "";
     var table_esb = "";
     var table_pcddr = "";
-    for (var i = 0; i < data.hours.length; i++){
+    for (var i = 0; i < data.hours.length; i++) {
         table_pcea += "<tr>";
         table_esb += "<tr>";
         table_pcddr += "<tr>";
-        for (var j = 0; j < data.pcea.length; j++){
+        table_pcea += "<td>" + data.hours[i] + "</td>"
+        table_esb += "<td>" + data.hours[i] + "</td>"
+        table_pcddr += "<td>" + data.hours[i] + "</td>"
+        for (var j = 0; j < data.pcea.length; j++) {
             console.log(i, j)
             table_pcea += "<td>" + data.pcea[j][i] + "</td>";
         }
-        for (var k = 0; k < data.esb.length; k++){
+        for (var k = 0; k < data.esb.length; k++) {
             table_esb += "<td>" + data.esb[k][i] + "</td>";
         }
-        for (var x = 0; x < data.pcddr.length; x++){
+        for (var x = 0; x < data.pcddr.length; x++) {
             table_pcddr += "<td>" + data.pcddr[x][i] + "</td>";
         }
         table_pcea += "</tr>";
