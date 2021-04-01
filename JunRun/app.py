39102e077de25f31595a06144753594a1f604ddd
Isaac <tam.isaac333@gmail.com>
2021-03-31 22:18:01 -0400

    Added Flask implementation

--- JunRun/app.py
+++ JunRun/app.py
@@ -1,14 +1,44 @@
-from flask import Flask, render_template, Blueprint
-#import Chart from 'chart.js'
+########  imports  ##########
+from flask import Flask, jsonify, request, render_template
+import pandas as pd
+import weather_scraper
+
+
 app = Flask(__name__)
 
-#@app.route("/home")
+# data = list(range(1,300,3))
+# print (data)
+
 @app.route('/')
+def home_page():
+    example_embed='This string is from python'
+    return render_template('index.html', embed=example_embed)
 
-def home():
-    return render_template("index.html")
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Success', 200
 
+######## Data fetch ############
+@app.route('/getdata', methods=['GET','POST'])
+def data_get():
+    
+    if request.method == 'POST': # POST request
+        print(request.get_text())  # parse as text
+        return 'OK', 200
+    
+    else: # GET request
+        weather_scraper.getData(); 
+        df = pd.read_csv('weather_data.csv');
+        print(df)
+        data = df.to_json(orient= "split")
+        return jsonify(data)
+        # return 't_in = %s ; result: %s ;'%(index_no, data[int(index_no)])
 
-if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8000, debug=True)
-    app.run()
+app.run(debug=True)
